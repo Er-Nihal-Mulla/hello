c22c0690b4fd1d7024440c9cf463c965dbddd73d
Neel9784 <neel9784@gmail.com>
2020-12-06 15:24:58 +0530

    Second change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import BootstrapNavbar from './component/Navbar/BootstrapNavbar'
+import {Helmet} from 'react-helmet'
 
+
+const TITLE = 'DeployDemo'
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Helmet>
+      <title>{TITLE}</title>
+    </Helmet>
+      <BootstrapNavbar/>
     </div>
   );
 }
